77106240340dd855fd17a214c0f043762798ebec
Breephyl <mukoyabrenda12@gmail.com>
2022-06-30 12:59:21 -0700

    betty docs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * main -> A function, print programming is like building a multilingual puzzle
+ * Return: success always 0
+ */
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle\n");
